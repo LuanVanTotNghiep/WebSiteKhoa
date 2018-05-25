2cef3826ba30ecc74dab28725f12b8a683930e55
trungthangdk <trungthangdk@gmail.com>
2018-05-25 13:28:39 +0700

    tao portlet

---
+++ portlets/Top-link-portlet/docroot/WEB-INF/src/com/dhnl/cntt/portlet/action/ActionLink.java
@@ -0,0 +1,11 @@
+package com.dhnl.cntt.portlet.action;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ActionLink
+ */
+public class ActionLink extends MVCPortlet {
+ 
+
+}
